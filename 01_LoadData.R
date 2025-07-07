5dca8203a6f2a3d18105d09b515f6b143145b301
Angelaric <angelbogdanovgrigorov@gmail.com>
2025-07-07 12:01:04 +0300

    Modify 01_LoadData.R

--- 01_LoadData.R
+++ 01_LoadData.R
@@ -11,28 +11,6 @@ library(mapview)
 
 # Load Data
 
-################### ANGEL RASTERS
-# setwd("Z:/CAA2025/()!_Krunos_horos/!R/AqranusGitHub") # I am not sure it will work... 
-# 
-# # The DEM of Bulgaria which we will need. 
-# # [AS] WHY? Explanation needed.
-# # Because it is too heavy to be uploaded in GitHub,
-# # I load it from my computer, but it can be downloaded from...
-# 
-# # [AS] if you have a custom-made raster, zip it up into a file and deposit in Zenodo, sciencedata, googledrive, etc.
-# # listing their provenance etc. you will need it for the paper anyway.
-# 
-# Territory <- raster("Z:/CAA2025/()!_Krunos_horos/Balc_DEM.tif")
-# 
-# # This is the DEM we need for the LCP Analysis. It includes river Tundzha as a barrier and the fords for crossing the river.
-# # But this file is too big for GitHub and I will ignore it. 
-# # I don't know any other way how to share it. It is just a DEM. 
-# # You said that you can use some internet generated. 
-# # A simple DEM for the region is enough too. It is not necessary to have the River as a barrier.
-# 
-# Territory2 <- raster("data/large/CityTerTonsosBarr.tif") 
-
-
 ######################  VECTORS
 
 # Create the locations Lufisa, Farui and Istibuni
@@ -40,8 +18,8 @@ library(mapview)
 Locations <- data.frame(place = c("Lufisa", 
                                   "Farui",
                                   "Istibuni"),
-                        longitude=c(24.715602, # 1 Lufisa
-                                    25.626568, # 2 Farui
+                        longitude=c(24.715602,  # 1 Lufisa
+                                    25.626568,  # 2 Farui
                                     23.816906), # 3 Istibuni
                         
                         
@@ -54,11 +32,14 @@ Locations <- data.frame(place = c("Lufisa",
   
   st_transform(crs = 32635) # Transform the points to CRS EPSG 32635
 
+
+
 # This is the road system with all Wendel's (2005) categories as shapefiles. 
-# It is georeferenced and digitised by me.
+# It is georeferenced and digitised by us.
 
 WendelRoads <- st_read(here("data", "WendelRoads2.shp"))
 
+# Because of the specifications of sfnetwork package we need to: 
 # Here follows the loading of the Wendel's road system converted from lines to points.
 # The file is created in QGIS with Convert Lines to Polygons. 
 # In field "Lines" the vectorised Wendel's roads file is loaded. 
@@ -71,11 +52,12 @@ WendelRoads <- st_read(here("data", "WendelRoads2.shp"))
 WendelRoadsPoints <- st_read(here("data", "WendelRoads2Points.shp"))
 
 
-# Create dataframe with lat long data for Beroe, Tarnovo and Strinava 
-# [AS] WHY? Explanation needed.
+# Create dataframe with lat long data for Beroe, Tarnovo and Tsareva Livada 
+# These destinations/origins are used in the additional LCP and Shortest Road Network Analysis.
+
 Origo_Destino <- data.frame(place = c("Beroe", 
                                       "Tarnovo",
-                                      "Strinava"),
+                                      "Tsareva Livada"),
                             longitude=c(25.630772, 25.652008, 25.442040),
                             latitude=c(42.428833, 43.083266, 42.953527))%>%
   st_as_sf(coords = c("longitude", "latitude"), crs=4326)%>% # Convert the data frame to an sf object             
@@ -84,13 +66,18 @@ Origo_Destino <- data.frame(place = c("Beroe",
 
 
 # Because the claim is that the Romans and after the Roman Period the roads were following the ridges of the mountains, 
-# so this is the file with the extracted ridges, which will force the model to use them instead of the valleys
-# [AS : nice when you explain Why. Consider adding the provenance of this dataset]
+# so this is the file with the extracted ridges, which will force the model to use them instead of the valleys. 
+# The Dataset is created in r.geomorphon in GRAS GISS as follows: 
+# For this purpose we create a new file in r.geomorphon elevation="here we load our DEM" forms="BalcFormsR10Scip05Flat5" search=10 skip=5 flat=5 dist=0 step=0 start=0
+# From this file in GRAS GIS with r.pamcalc we will cut only the ridges and peaks. The formula is as follows r.mapcalc expression ”Ridges= if((BalcFormsR10Scip05Flat5 >= 2 && BalcFormsR10Scip05Flat5 <= 3), 1, null())”, 
+# where 2 is the code for the peak, and 3 for the ridge. You get code 1, and everything else with code 0. Export this file to geotiff.
+# Then we make the geotiff. into vectors with r.to.vect (r.to.vect input=Ridges@CFMN output=RidgesVector@CFMN type=area) and export them as a shapefile SR2.shp
+
 Ridges <- st_read("data/SR2.shp") %>%
   as_Spatial()
 
 
-##############  ADELA RASTERS
+##############  RASTERS
 
 # Create a DEM of Territory and Territory2 using `geodata` library
 
