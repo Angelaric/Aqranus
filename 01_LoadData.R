a142fe71e256f0582aa07708b3f7224aea1cc39a
Angelaric <angelbogdanovgrigorov@gmail.com>
2025-07-07 12:15:29 +0300

    Compleating 01_LoadData.R

--- 01_LoadData.R
+++ 01_LoadData.R
@@ -1,5 +1,4 @@
 # All the library we need
-
 library(sf)
 library(dplyr)
 library(sfnetworks)
@@ -14,7 +13,6 @@ library(mapview)
 ######################  VECTORS
 
 # Create the locations Lufisa, Farui and Istibuni
-
 Locations <- data.frame(place = c("Lufisa", 
                                   "Farui",
                                   "Istibuni"),
@@ -36,7 +34,6 @@ Locations <- data.frame(place = c("Lufisa",
 
 # This is the road system with all Wendel's (2005) categories as shapefiles. 
 # It is georeferenced and digitised by us.
-
 WendelRoads <- st_read(here("data", "WendelRoads2.shp"))
 
 # Because of the specifications of sfnetwork package we need to: 
@@ -48,13 +45,11 @@ WendelRoads <- st_read(here("data", "WendelRoads2.shp"))
 # In this way we have converted the shapefile from line to points with point in every 500 m. 
 # Then leave only the fields "FID", "Shape", "Type", "Shape Lenght" and "PT ID". 
 # We will need them. 
-
 WendelRoadsPoints <- st_read(here("data", "WendelRoads2Points.shp"))
 
 
 # Create dataframe with lat long data for Beroe, Tarnovo and Tsareva Livada 
 # These destinations/origins are used in the additional LCP and Shortest Road Network Analysis.
-
 Origo_Destino <- data.frame(place = c("Beroe", 
                                       "Tarnovo",
                                       "Tsareva Livada"),
@@ -72,7 +67,6 @@ Origo_Destino <- data.frame(place = c("Beroe",
 # From this file in GRAS GIS with r.pamcalc we will cut only the ridges and peaks. The formula is as follows r.mapcalc expression ”Ridges= if((BalcFormsR10Scip05Flat5 >= 2 && BalcFormsR10Scip05Flat5 <= 3), 1, null())”, 
 # where 2 is the code for the peak, and 3 for the ridge. You get code 1, and everything else with code 0. Export this file to geotiff.
 # Then we make the geotiff. into vectors with r.to.vect (r.to.vect input=Ridges@CFMN output=RidgesVector@CFMN type=area) and export them as a shapefile SR2.shp
-
 Ridges <- st_read("data/SR2.shp") %>%
   as_Spatial()
 
@@ -80,8 +74,6 @@ Ridges <- st_read("data/SR2.shp") %>%
 ##############  RASTERS
 
 # Create a DEM of Territory and Territory2 using `geodata` library
-
-# [AS] If I know your DEM resolution and extent, I may be able to replicate it.
 # Load rasters
 library(geodata)
 
@@ -92,8 +84,7 @@ plot(dem)
 # Territory specified by lat/long at 3s resolution, may need mosaicing
 hires_dem <- elevation_3s(lon = 23.383056, lat = 42.666944, path = ".")
 
-# [AS]: if you provide extent, I will generate Territory2 in a reproducible way
-# now I am extrapolating extent from your Locations
+# Now we are extrapolating extent from the Locations
 extent <- Origo_Destino %>% 
   st_buffer(15000) %>% 
   st_union() %>% 
